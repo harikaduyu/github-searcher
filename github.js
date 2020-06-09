078aa61726d4b6911846e72686bef2bb4e39acd2
Harika Duyu <h.duyu@rebuy.com>
2020-06-09 08:04:45 +0200

    fetching user data

---
+++ github.js
@@ -0,0 +1,16 @@
+class GitHub {
+  constructor() {
+    this.client_id = '205363b7d51f9c78933b';
+    this.client_secret = '3828727e5f26e4c3b70de847292c9ee83c7b2b97';
+  }
+
+  async getUser(user) {
+    const profileResponse =  await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    return {
+      profile
+    }
+
+  }
+}
